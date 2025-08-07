16c949b41c87163602cfe220eaea733f64b3b5db
edouard-todorov-formation <edouard.todorov@laplateforme.io>
2025-08-07 02:20:54 +0200

    ajout modif fin de journée de atif

--- Public/registration.php
+++ Public/registration.php
@@ -1,12 +1,19 @@
 <?php
-    $error;
-
+    require_once "../config/database.php";
+    var_dump(dbConnexion());
+    $errors = [];
+    // =========================================
+    // condition qui contient la logique de traitement du formulaire quand on recoit une request POST
+    // ==========================================
     if ($_SERVER["REQUEST_METHOD"] === "POST");{
+        //recuperation des données d'un formulaire
+        //nettoyage des données du formulaire
         $username = htmlspecialchars(trim($_POST["username"]));
         $email = htmlspecialchars(trim($_POST["email"]));
-        $password = ($_POST["password"]);
-        $confirmPassword = ($_POST["confirmPassword"]);
-//validation des données
+        $password = $_POST["password"];
+        $confirmPassword = $_POST["confirmPassword"];
+
+//validation des données************************************
     //validation username
     //valide que le champ soit remplis
     if (empty($username)) {
@@ -18,11 +25,11 @@
     }elseif (strlen($username) > 55) {
         $errors[] = "max 55 carac";
     }
-    //var_dump($errors);
+   
     //validation email
     if (empty($email)) {
         $errors[] = "email obligatoire ! ( connard )";
-    //function filter_var(cible du traitement ex:$email, filtre ex:FILTER_VALIDATE MAIL ou FILTER_VALIDATE URL)
+    //function filter_var(cible du traitement ex:$email, filtre ex:FILTER_VALIDATE_MAIL ou FILTER_VALIDATE_URL ou FILTER_VALIDATE_INT ou FILTER_VALIDATE_BOOLEAN...)
     }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $errors[] = "votre adresse ne correspond au format mail classique";
     }
@@ -49,12 +56,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../assets/style/style.css">
 </head>
 <body>
 <!---------------  FORM SECTION ------------------------------------------------------------>
     <section class="main-container">
         <form action="" method="POST">
+            <?php
+                foreach ($errors as $error) {
+                    echo $error;
+                }
+            ?>
             <input type="text" name="username" id="username" placeholder="Username" required>
             <input type="email" name="email" id="email" placeholder="Email" required>
             <input type="password" name="password" id="password" placeholder="Password" required>
